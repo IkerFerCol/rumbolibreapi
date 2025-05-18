5f5096f248749441a2f12c46db64b31075f02e32
Iker Fernández <148960996+IkerFerCol@users.noreply.github.com>
2025-05-18 17:44:35 +0200

    implementacion de get de vuelo random en el controller

--- src/main/java/com/iker/rumbolibreapi/controller/vueloController.java
+++ src/main/java/com/iker/rumbolibreapi/controller/vueloController.java
@@ -3,12 +3,14 @@ package com.iker.rumbolibreapi.controller;
 import com.iker.rumbolibreapi.model.vuelo;
 import com.iker.rumbolibreapi.model.vueloRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Random;
 
 @RestController
 @RequestMapping("/vuelos")
@@ -42,4 +44,15 @@ public class vueloController {
         return vueloRepository
                 .findByCiudadOrigenOrCiudadDestinoOrAerolinea(ciudadOrigen, ciudadDestino, aerolinea);
     }
+
+    @GetMapping("/random")
+    public ResponseEntity<vuelo> getRandomVuelo() {
+        List<vuelo> vuelos = vueloRepository.findAll();
+        if (vuelos.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        vuelo vueloAleatorio = vuelos.get(new Random().nextInt(vuelos.size()));
+        return ResponseEntity.ok(vueloAleatorio);
+    }
+
 }
